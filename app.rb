2c84924e93a3d7fb3f41a17f879e47b63e995a26
gitvar <benvanderm@gmail.com>
2017-03-22 23:15:36 +0200

    Last commit for 4-part Blog series.

--- app.rb
+++ app.rb
@@ -1,12 +1,9 @@
 # app.rb
 
+require_relative 'monroe_framework'
 require_relative 'advice'
 
-class App
-  # def initialize
-  #   @message = ""
-  # end
-
+class App < MonroeFramework
   def call(env)
     case env['REQUEST_PATH']
     when '/'
@@ -24,58 +21,10 @@ class App
       end
     else
       status = '404'
-      headers = { "Content-Type" => 'text/html', "Content-Length" => '62' }
+      headers = { "Content-Type" => 'text/html', "Content-Length" => '91' }
       response(status, headers) do
         erb :not_found
       end
     end
   end
-
-  def response(status, headers, body = '')
-    body = yield if block_given?
-    [status, headers, [body]]
-  end
-
-  private
-
-  def erb(filename, local = {})
-    b = binding
-    # @message = local[:message]
-    message = local[:message]
-    path = File.expand_path("../views/#{filename}.erb", __FILE__)
-    content = File.read(path)
-    ERB.new(content).result(b)
-  end
 end
-
-# class HelloWorld
-#   def call(env)
-#     case env['REQUEST_PATH']
-#     when '/'
-#       # template = File.read("views/index.erb")
-#       # content = ERB.new(template)
-#       ['200', { "Content-Type" => "text/html" }, [erb(:index)]] # pass in a symbol, signifying which template to render.
-#     when '/advice'
-#       piece_of_advice = Advice.new.generate
-#       [
-#         '200',
-#         { "Content-Type" => 'text/html' },
-#         ["<html><body><h2><em>#{piece_of_advice}</em></h2></body></html>"]
-#       ]
-#     else
-#       [
-#         '404',
-#         { "Content-Type" => 'text/html', "Content-Length" => '48' },
-#         ["<html><body><h2>404 Not Found</h2></body></html>"]
-#       ]
-#     end
-#   end
-#
-#   private
-#
-#   def erb(filename)
-#     path = File.expand_path("../views/#{filename}.erb", __FILE__)
-#     content = File.read(path)
-#     ERB.new(content).result
-#   end
-# end
